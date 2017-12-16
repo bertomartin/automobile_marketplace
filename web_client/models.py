91f2e5a1c6e5b2fe2bd37351e7ed8f7809cb94f8
Stan Reduta <stanislaw.reduta@gmail.com>
2017-12-16 21:39:01 +0100

    Creates model classes for Offer attributes like engine capacity, body type.

--- web_client/models.py
+++ web_client/models.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 
 
 class Offer(models.Model):
+    # TODO: each field should be a foreign key to list of related values
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
     engine = models.CharField(max_length=20)
@@ -11,3 +12,15 @@ class Offer(models.Model):
 
     def get_absolute_url(self):
         return reverse('homepage')
+
+
+class BodyTypes(models.Model):
+    body_type = models.CharField(max_length=20, blank=False)
+
+
+class EngineTypes(models.Model):
+    engine_type = models.CharField(max_length=20, blank=False, unique=True)
+
+
+class EngineCapacities(models.Model):
+    engine_capacity = models.FloatField(blank=False, unique=True)
