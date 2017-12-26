1b9af7be3b6f1e55c354f6b0cc0da7511412c26c
Stan Reduta <stanislaw.reduta@gmail.com>
2017-12-26 10:39:10 +0300

    Registers models with django admin page. Adds sample 'manufacturer' data to db.

--- web_client/admin.py
+++ web_client/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Offer)
+admin.site.register(Manufacturer)
+admin.site.register(BodyTypes)
+admin.site.register(EngineTypes)
+admin.site.register(EngineCapacities)
