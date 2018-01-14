31a2da2624e7845b4de4b568fbbb502c22baf7c6
Stan Reduta <stanislaw.reduta@gmail.com>
2018-01-14 18:14:48 +0100

    Renames homepage script file.

--- web_client/templates/homepage/homepage.js
+++ web_client/templates/homepage/homepage.js
@@ -1,7 +1,13 @@
 var selected_list_item;
 
 $('.list-group-item').click(function () {
+    console.log(this.id);
     $(selected_list_item).removeClass('active');
     selected_list_item = this;
     $(this).addClass('active');
+});
+
+$('.share').on('shown.bs.modal', function () {
+    console.log('Generating url...');
+    $('#post-url').focus();
 });
