b16e3a67d65bd2ddc2674fee4fea9141cdc6755e
Stan Reduta <stanislaw.reduta@gmail.com>
2018-01-24 22:43:48 +0100

    Sticks 'view options' and 'ads' navbars to top of the page.

--- web_client/templates/homepage/homepage.js
+++ web_client/templates/homepage/homepage.js
@@ -78,3 +78,19 @@ $('#search-input').keyup(function () {
     }
 });
 
+
+$(function () {
+    var
+        $win = $(window),
+        $filter = $('.scroll-navbar'),
+        $filterSpacer = $('<div />', {
+            "class": "filter-drop-spacer",
+            "height": 0
+        });
+    $win.scroll(function () {
+        $filter.before($filterSpacer);
+        $filter.addClass("fix");
+
+    });
+});
+
